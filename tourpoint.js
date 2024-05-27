16cb91f0058b74a4f32fc340015bed2e441e50d2
SarochaPang <158019650+SarochaPang@users.noreply.github.com>
2024-05-27 17:41:55 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bangpu Recreation Center",
+        "type": "natural",
+        "level": 4.3
+      },
+      "geometry": {
+        "coordinates": [
+          100.6546525423297,
+          13.51736358983699
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Samut Prakan Observation",
+        "type": "natural",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          100.59898215144904,
+          13.59823837713266
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Khun Phaen House",
+        "type": "cultural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          100.62825551716469,
+          13.546381129382695
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Siam Fighting Fish Gallery",
+        "type": "special",
+        "level": 3.9
+      },
+      "geometry": {
+        "coordinates": [
+          100.56266727116491,
+          13.699965270779728
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Phra Samut Chedi",
+        "type": "special",
+        "level": 4.9
+      },
+      "geometry": {
+        "coordinates": [
+          100.58678332649725,
+          13.600272886400589
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Erawan Museum",
+        "type": "cultural",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.58910315583188,
+          13.628749505021958
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Asokaram",
+        "type": "special",
+        "level": 4.7
+      },
+      "geometry": {
+        "coordinates": [
+          100.60791611580527,
+          13.547430523892302
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Phra Chulachomklao Fort",
+        "type": "cultural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          100.58401473001629,
+          13.537643026188292
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Bangplee Yainai ",
+        "type": "special",
+        "level": 4.6
+      },
+      "geometry": {
+        "coordinates": [
+          100.71140300000053,
+          13.604411855632165
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bang Phli Floating Market",
+        "type": "special",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.71052727116711,
+          13.60371213923905
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
